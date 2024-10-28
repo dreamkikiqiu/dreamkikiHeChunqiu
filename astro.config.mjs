59b3b3afb13880de44716541bd1a778ac8c6b1c9
dreamkikiqiu <907741673@qq.com>
2024-10-28 14:56:35 +0800

    二叉树专题博客代码提交

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://dreamkikiqiu.github.io/",
-  base: "/dreamkikiqiu.github.io",
+  site: "https://dreamkikiqiuHeChunqiu/",
+  base: "/dreamkikiqiuHeChunqiu",
   trailingSlash: 'always',
   integrations: [
     tailwind(),
