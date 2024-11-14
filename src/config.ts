20e930bf025587c19670238d9d0f555c4efbd0b6
dreamkikiqiu <907741673@qq.com>
2024-11-14 14:48:20 +0800

    维护一下网站

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import { LinkPreset } from './types/config'
 
 export const siteConfig: SiteConfig = {
   title: 'DreamKiki',
-  subtitle: 'Demo Site',
+  subtitle: 'Just another blog',
   lang: 'zh_CN',         // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko'
   themeColor: {
     hue: 250,         // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
@@ -85,7 +85,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'bilibili',
       icon: 'fa6-brands:bilibili',
-      url: 'https:www.bilibili.com',
+      url: 'https://www.bilibili.com',
     }
   ],
 }
