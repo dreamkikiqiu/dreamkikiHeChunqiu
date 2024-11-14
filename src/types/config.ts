0f44df6cec9c53f54493c15500848a7e944951e4
dreamkikiqiu <907741673@qq.com>
2024-11-14 16:27:42 +0800

    维护一下网站

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,4 +1,4 @@
-import type { AUTO_MODE, DARK_MODE, LIGHT_MODE } from '@constants/constants'
+import type { LIGHT_MODE, DARK_MODE, AUTO_MODE } from "@constants/constants"
 
 export type SiteConfig = {
   title: string
@@ -13,16 +13,7 @@ export type SiteConfig = {
   banner: {
     enable: boolean
     src: string
-    position?: 'top' | 'center' | 'bottom'
-    credit: {
-      enable: boolean
-      text: string
-      url?: string
-    }
-  }
-  toc: {
-    enable: boolean
-    depth: 1 | 2 | 3
+    position?: string
   }
 
   favicon: Favicon[]
@@ -38,6 +29,8 @@ export enum LinkPreset {
   Home = 0,
   Archive = 1,
   About = 2,
+  Gallery = 3,
+  Friends = 4,
 }
 
 export type NavBarLink = {
@@ -67,22 +60,4 @@ export type LicenseConfig = {
   url: string
 }
 
-export type LIGHT_DARK_MODE =
-  | typeof LIGHT_MODE
-  | typeof DARK_MODE
-  | typeof AUTO_MODE
-
-export type BlogPostData = {
-  body: string
-  title: string
-  published: Date
-  description: string
-  tags: string[]
-  draft?: boolean
-  image?: string
-  category?: string
-  prevTitle?: string
-  prevSlug?: string
-  nextTitle?: string
-  nextSlug?: string
-}
+export type LIGHT_DARK_MODE = typeof LIGHT_MODE | typeof DARK_MODE | typeof AUTO_MODE
